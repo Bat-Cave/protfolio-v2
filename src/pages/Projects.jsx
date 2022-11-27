4136a4cd46a54c99bcf5833374e7d68f5644282e
Richard Hancock <richardl@hancock1.com>
2022-11-27 02:05:55 -0700

    added todo for projects

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,5 +1,8 @@
 import SectionWrapper from "../components/SectionWrapper";
 
+//todo: go through coding folder and fild the projects you've made
+//todo: add necessary projects to github for easier referencing
+
 const Projects = () => {
   return <SectionWrapper>Projects</SectionWrapper>;
 };
